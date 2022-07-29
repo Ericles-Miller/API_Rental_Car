ad739a97355aa288539aace05e67e99dc0aabb74
Éricles Miller <ericlesmiller15@gmail.com>
2022-07-29 16:59:09 -0300

    modulo 2 cap 2 aula 5  types

--- TypeScript-padrao/src/routes/categories.routes.ts
+++ TypeScript-padrao/src/routes/categories.routes.ts
@@ -1,19 +1,24 @@
 import { Router } from 'express'; //importo da biblioteca express
-
+// importando car de rotas 
 const categoriesRoutes = Router();
+
+
+// importando lybralys API 
+import {v4 as uuid4} from 'uuid';
 
 // arrays e var globais 
 const categories = [];
 
 
-categoriesRoutes.post("/categories", (request, response) =>{
+categoriesRoutes.post("/", (request, response) =>{
     const {name,description} = request.body;
-    const categorie = {
+    const category = {
+        id: uuid4(),    // -- nao esqueca do parenteses 
         name,
         description
     };
 
-    categories.push(categorie);
+    categories.push(category);
 
     return response.status(201).send()
 });
