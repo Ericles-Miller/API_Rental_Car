a9c6cf32c3a0ed2abef930f2ea2563f20ea5e1ca
Éricles Miller <ericlesmiller15@gmail.com>
2022-08-01 13:52:11 -0300

    types.modulo.2.2.8

--- TypeScript-padrao/src/routes/categories.routes.ts
+++ TypeScript-padrao/src/routes/categories.routes.ts
@@ -9,11 +9,14 @@ import { CategoriesRepository } from '../repositories/CategoriesRepository';
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-// arrays e var globais ;
-const categories: Category[] = []; // instance class ;
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
+
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    if(categoryAlreadyExists) {
+        return response.status(400).json({error:"Category already exists!"});
+    }
     
     categoriesRepository.create({name, description});
     return response.status(201).send();
