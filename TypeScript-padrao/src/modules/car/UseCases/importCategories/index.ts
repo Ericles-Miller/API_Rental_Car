1caee6b71b834ef833135f52a472812ee8df32de
Éricles Miller <ericlesmiller15@gmail.com>
2022-08-04 12:46:30 -0300

    types.modulo.2.5.3

---
+++ TypeScript-padrao/src/modules/car/UseCases/importCategories/index.ts
@@ -0,0 +1,8 @@
+import { ImportCategoryController } from "./importCategoryController";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+const importCategoryUseCase = new ImportCategoryUseCase();
+const importCategoryController = new ImportCategoryController(importCategoryUseCase);
+
+
+export{ importCategoryController};
