7b7213c6dbb677e19cd83a0382e5d0b1f9ba815c
Éricles Miller <ericlesmiller15@gmail.com>
2022-08-18 14:54:54 -0300

    types.module.3.2.6

--- TypeScript-padrao/src/modules/car/model/category.ts
+++ TypeScript-padrao/src/modules/car/model/category.ts
@@ -1,9 +1,24 @@
 import { v4 as uuidV4 } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
 
+
+@Entity("categories") // relacao feita com a tabela categories do banco de dados
+// farei agora referencias de cada variavel a coluna da tabela categories
 class Category {
-    id?: string; // not exists in typescript uuid4
+    /**
+     * como definimos as variaveis da classe igual ao nome da entidades da tabela 
+     * n'ao precisaremos definir para o nome 
+     */
+    @PrimaryColumn() // defini que e primary key
+    id?: string; 
+
+    @Column()
     name: string;
+
+    @Column()
     description: string;
+
+    @CreateDateColumn()
     created_at: Date;
 
     constructor() {
