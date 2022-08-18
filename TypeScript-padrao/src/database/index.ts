029d3b2b1b93d103d1b8bf9e35221dc224ee7198
Éricles Miller <ericlesmiller15@gmail.com>
2022-08-18 14:09:07 -0300

    typesmodule.3.2.1.2

--- TypeScript-padrao/src/database/index.ts
+++ TypeScript-padrao/src/database/index.ts
@@ -1,15 +1,19 @@
-import {DataSource} from "typeorm";
+// import {DataSource} from "typeorm";
+// 
+// export const dataSource = new DataSource({
+//     type: "mssql",
+//     host: "localhost",
+//     username:"sa",
+//     password:"@18er0821",
+//     database:"rentex",
+//     migrations:["./src/database/migrations/*.ts"],
+//     extra:{
+//         trustServerCertificate: true,
+//     }
+// });
+// 
+// dataSource.initialize();
 
-export const dataSource = new DataSource({
-    type: "mssql",
-    host: "localhost",
-    username:"sa",
-    password:"@18er0821",
-    database:"rentex",
-    migrations:["./src/database/migrations/*.ts"],
-    extra:{
-        trustServerCertificate: true,
-    }
-});
+import { createConnection } from "typeorm";
 
-dataSource.initialize();
+createConnection();
