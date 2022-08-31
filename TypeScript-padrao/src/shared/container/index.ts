9f46e108e914fec35cde31346ca09672b00ea34d
Éricles Miller <ericlesmiller15@gmail.com>
2022-08-31 14:44:54 -0300

    types.module.4.1.6

--- TypeScript-padrao/src/shared/container/index.ts
+++ TypeScript-padrao/src/shared/container/index.ts
@@ -1,10 +1,10 @@
 import {container} from 'tsyringe';
-import { UsersRepository } from '../../modules/accounts/repositories/implementations/UserRepository';
-import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
-import { ICategoriesRepository } from '../../modules/car/repositories/ICategoriesRepository';
-import { CategoriesRepository } from '../../modules/car/repositories/implementations/CategoriesRepository';
-import { SpecificationsRepository } from '../../modules/car/repositories/implementations/SpecificationRespository';
-import { ISpecificationRepository } from '../../modules/car/repositories/ISpecificationRepository';
+import { UsersRepository } from '@modules/accounts/repositories/implementations/UserRepository';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { ICategoriesRepository } from '@modules/car/repositories/ICategoriesRepository';
+import { CategoriesRepository } from '@modules/car/repositories/implementations/CategoriesRepository';
+import { SpecificationsRepository } from '@modules/car/repositories/implementations/SpecificationRespository';
+import { ISpecificationRepository } from '@modules/car/repositories/ISpecificationRepository';
 
 /**
  * vamos passar a interface Icategoryrepository
