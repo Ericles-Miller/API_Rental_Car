797da672f88c51d0787fc779702830f8bcc62753
Éricles Miller <ericlesmiller15@gmail.com>
2022-09-05 16:15:01 -0300

    types.module.4.3.6

--- TypeScript-padrao/src/shared/infra/http/server.ts
+++ TypeScript-padrao/src/shared/infra/http/server.ts
@@ -1,38 +1,39 @@
-import "reflect-metadata"; // 
-import  express, {NextFunction,Request,Response} from "express";
-import "express-async-errors";
-import { AppError } from "@shared/errors/AppError";
-import { router } from "./routes";
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
-import swaggerFile from "../../../swagger.json";
-import "@shared/infra/typeorm"; // importo as cpnfigs do database
-import "@shared/container";
 
+import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
 
+import swaggerFile from '../../../swagger.json';
+import { router } from './routes';
 
+import '@shared/container';
+
+createConnection();
 const app = express();
 app.use(express.json());
 
-// rota responsavel por descrever a doc da API 
-// swagger.UI cria um servidor para acessar a doc 
-// swagger.Ui.setup cria um arquivo em json 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+/* rota responsavel por descrever a doc da API
+   swagger.UI cria um servidor para acessar a doc
+   swagger.Ui.setup cria um arquivo em json */
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-/*middle error */
-app.use(
-    (err:Error, request:Request, response:Response, next:NextFunction) => {
-    if(err instanceof AppError){
-        return response.status(err.statusCode).json({
-            message: err.message
-        });
-    }
-    console.log(err)
-    return response.status(500).json({
-        status:"error",
-        message: `Internal server error - ${err.message}`
+/* middle error */
+app.use((err:Error, request:Request, response:Response, next:NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
     });
-})
+  }
+  console.log(err);
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => console.log('Server is running!'));
