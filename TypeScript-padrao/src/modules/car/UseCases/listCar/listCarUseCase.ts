f997bece17f3e9b37c4862075ac17b255ce15706
Éricles Miller <ericlesmiller15@gmail.com>
2022-09-08 10:23:58 -0300

    types.module.4.3.7

--- TypeScript-padrao/src/modules/car/UseCases/listCar/listCarUseCase.ts
+++ TypeScript-padrao/src/modules/car/UseCases/listCar/listCarUseCase.ts
@@ -2,9 +2,9 @@ import { Car } from '@modules/car/infra/typeorm/entities/car';
 import { ICarsRepository } from '@modules/car/repositories/ICarsRepository';
 
 interface IRequest {
-    category_id: string,
-    brand: string,
-    name:string,
+    category_id?: string,
+    brand?: string,
+    name?:string,
 }
 
 class ListCarsUseCase {
@@ -12,8 +12,7 @@ class ListCarsUseCase {
     private carsRepository: ICarsRepository,
   ) {}
 
-  async execute({
-    category_id, brand, name }: IRequest) : Promise<Car[]> {
+  async execute({ category_id, brand, name }: IRequest) : Promise<Car[]> {
     const cars = await this.carsRepository.findAvailable(brand, category_id, name);
     return cars;
   }
--- TypeScript-padrao/src/modules/car/repositories/inmemory/CarsRepositoryInMemory.ts
+++ TypeScript-padrao/src/modules/car/repositories/inmemory/CarsRepositoryInMemory.ts
@@ -31,7 +31,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
   async findAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]> {
     const all = this.cars.filter((car) => {
       if (
-        car.available === true && ((brand && car.brand === brand) || (
+        car.available === true || ((brand && car.brand === brand) || (
           category_id && car.category_id === category_id) || (
           name && car.name === name))
       ) {
