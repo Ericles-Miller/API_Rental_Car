689e12e583f8ad9e828525ea6afa7196137f7749
Éricles Miller <ericlesmiller15@gmail.com>
2022-09-09 14:38:50 -0300

    types.module.4.3.15

---
+++ TypeScript-padrao/src/modules/car/infra/typeorm/repositories/CarsImageRepository.ts
@@ -0,0 +1,24 @@
+import { ICarsImageRepository } from '@modules/car/repositories/ICarsImageRepitory';
+import { getRepository, Repository } from 'typeorm';
+
+import { CarImage } from '../entities/CarImage';
+
+class CarsImagerepository implements ICarsImageRepository {
+    private repository: Repository<CarImage>;
+
+    constructor() {
+      this.repository = getRepository(CarImage);
+    }
+
+    async create(car_id: string, image_name: string): Promise<CarImage> {
+      const carImage = this.repository.create({
+        car_id,
+        image_name,
+      });
+
+      await this.repository.save(carImage);
+      return carImage;
+    }
+}
+
+export { CarsImagerepository };
