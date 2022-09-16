60d5a3f5c97fb96b50531b56a79917a6cdfe25e5
Éricles Miller <ericlesmiller15@gmail.com>
2022-09-16 12:48:01 -0300

    types.module.5.1.5

--- TypeScript-padrao/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ TypeScript-padrao/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -12,8 +12,8 @@ class RentalsRepository implements IRentalsRepository {
   }
 
   async findByCar(car_id: string): Promise<Rental> {
-    const openByCar = await this.repository.findOne({ 
-      where: { car_id, end_date: null } 
+    const openByCar = await this.repository.findOne({
+      where: { car_id, end_date: null },
     });
     return openByCar;
   }
@@ -44,6 +44,11 @@ class RentalsRepository implements IRentalsRepository {
   async findById(id: string): Promise<Rental> {
     const rental = await this.repository.findOne(id);
     return rental;
+  }
+
+  async findByUser(user_id: string): Promise<Rental[]> {
+    const rentals = await this.repository.find({ user_id });
+    return rentals;
   }
 }
 
