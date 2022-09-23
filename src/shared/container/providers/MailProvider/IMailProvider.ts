72955ee5920799512c0260213cf0a5626816e590
Éricles Miller <ericlesmiller15@gmail.com>
2022-09-23 16:34:28 -0300

    v3.2

--- src/shared/container/providers/MailProvider/IMailProvider.ts
+++ src/shared/container/providers/MailProvider/IMailProvider.ts
@@ -1,5 +1,5 @@
 interface IMailProvider {
-    sendEmail(to: string, subject: string, body: string): Promise<void>;
+    sendEmail(to: string, subject: string, variables: any, path: string): Promise<void>;
 }
 
 export { IMailProvider };
