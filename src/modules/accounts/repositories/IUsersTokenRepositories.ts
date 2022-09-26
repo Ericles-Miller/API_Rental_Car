a27d8b7faf78a3a4d68231aafbaa6e31002228de
Éricles Miller <ericlesmiller15@gmail.com>
2022-09-26 17:19:14 -0300

    v3.4

--- src/modules/accounts/repositories/IUsersTokenRepositories.ts
+++ src/modules/accounts/repositories/IUsersTokenRepositories.ts
@@ -5,6 +5,7 @@ interface IUsersTokenRepository {
     create({ user_id, refresh_token, expires_date }: ICreateUserTokenDTO) : Promise<UserTokens>
     findByUserIdAndToken(userId: string, refresh_token:string): Promise<UserTokens>
     deleteById(id: string): Promise<void>
+    findByRefreshToken(refresh_token:string):Promise<UserTokens>;
 }
 
 export { IUsersTokenRepository };
