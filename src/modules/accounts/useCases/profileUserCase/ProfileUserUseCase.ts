d1e6182f3f888eeed736b556a6c1a35b405d5d39
Éricles Miller <ericlesmiller15@gmail.com>
2022-10-25 15:52:41 -0300

    types.module.5.2

--- src/modules/accounts/useCases/profileUserCase/ProfileUserUseCase.ts
+++ src/modules/accounts/useCases/profileUserCase/ProfileUserUseCase.ts
@@ -1,19 +1,17 @@
+import { User } from '@modules/accounts/infra/typeorm/entities/user';
+import { IUsersRepository } from '@modules/accounts/Repositories/IUsersRepository';
 import { inject, injectable } from 'tsyringe';
-
-import { IUserResponseDTO } from '@modules/accounts/dtos/IUserResponseDTO';
-//import { UserMap } from '@modules/accounts/mapper/UserMap';
-import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 
 @injectable()
 class ProfileUserUseCase {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
   ) {}
-  async execute(id: string): Promise<IUserResponseDTO> {
+  async execute(id: string): Promise<User> {
     const user = await this.usersRepository.findById(id);
 
-    return UserMap.toDTO(user);
+    return user;
   }
 }
 
