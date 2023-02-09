ea5cba44afa6f03881157afb7756d969c01bfa3d
Ericles-Miller <ericlesmiller15@gmail.com>
2023-02-09 09:42:36 -0300

    add correction

--- src/shared/infra/typeorm/migrations/1660921341948-CreateCategories.ts
+++ src/shared/infra/typeorm/migrations/1660921341948-CreateCategories.ts
@@ -1,37 +1,37 @@
-import {MigrationInterface,QueryRunner,Table} from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
 export class CreateCategories1660682202109 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name: "categories",
-                columns:[
-                {
-                    name:"id",
-                    type:"uuid",
-                    isPrimary: true
-                },
-                {
-                    name: "name",
-                    type: "varchar",
-                },
-                {
-                    name:"description",
-                    type:"varchar",
-                },
-                {
-                    name: "created_at",
-                    type:"timestamp",
-                    default: "now()"
-                }
-                ],
-            })
-        );
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()"
+          }
+        ],
+      })
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("categories");
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
 
 }
